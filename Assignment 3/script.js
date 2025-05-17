fd6431d587c30b17a35ff238e52f38c5e0e54b23
s3914501 <s3914501@student.rmit.edu.au>
2025-05-17 16:00:45 +1000

    updates

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -5,4 +5,18 @@ function moveSlide(direction) {
   const slides = slider.children.length;
   currentSlide = (currentSlide + direction + slides) % slides;
   slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+  updateDots();
+}
+
+function goToSlide(slideIndex) {
+  currentSlide = slideIndex;
+  const slider = document.getElementById("slider");
+  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+  updateDots();
+}
+
+function updateDots() {
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[currentSlide].classList.add("active");
 }
